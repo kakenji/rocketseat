592cc542c3e92cf0155caa6ad1b5a37dc1b51136
Kayky Kenji <88341735+kakenji@users.noreply.github.com>
2025-09-09 22:54:38 -0300

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import http from 'node:http';
+
+const server = http.createServer((req, res) => {
+    const { method, url } = req;
+
+    if(method === 'GET' && url === '/users'){
+        return res.end('Listagem de usuários');
+    }
+    
+    if(method === 'POST' && url === '/users'){
+        return res.end('Criação de usuário')
+    }
+
+    console.log(method, url);
+    return res.end('hello world');
+
+});
+
+server.listen(3333);
