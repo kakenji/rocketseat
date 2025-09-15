9a917455c7fc9acf6ff42da89019bcecf6c4cd75
kayky <kaykyjk01@gmail.com>
2025-09-15 10:29:11 -0300

    .

--- src/server.js
+++ src/server.js
@@ -1,35 +1,23 @@
 import http from 'node:http';
 import { json } from './middlewares/json.js';
-import { Database } from './database.js';
-import { randomUUID } from 'node:crypto';
-
-const database = new Database();
+import { routes } from './routes.js';
 
 const server = http.createServer(async (req, res) => {
     const { method, url } = req;
 
     await json(req, res);
 
-    if(method === 'GET' && url === '/users'){
-        const users = database.select('users');
-        return res .end(JSON.stringify(users));
+    const route = routes.find(route => {
+        return route.method === method && route.path.test(url);
+    })
+
+    // console.log(route);
+
+    if(route) {
+        const routeParams = req.url.match(route.path);
+        console.log(routeParams);
+        return route.handler(req, res);
     }
-    
-    if(method === 'POST' && url === '/users'){
-        const { name, email } = req.body;
-
-        const user = {
-            id: randomUUID(),
-            name,
-            email
-        }
-        database.insert('users', user)
-        
-
-        return res.writeHead(201).end();
-    }
-
-    console.log(method, url);
     return res.writeHead(404).end();
 
 });
